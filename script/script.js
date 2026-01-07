c6fe4270e3dd13063160e6a383a1061b218ccc1a
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-01-07 10:27:59 +0100

    Added user objects

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const user1 = {
+  name: "Karl",
+  totalScore: 501,
+  score: 0,
+  scores: [],
+  isMyTurn: true,
+  throwCounter: 0,
+};
+
+const user2 = {
+  name: "Guest",
+  totalScore: 501,
+  score: 0,
+  scores: [],
+  isMyTurn: true,
+  throwCounter: 0,
+};
