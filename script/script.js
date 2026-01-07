5ba1bc3d4fa4f2e56257da03c9487acc749b9256
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-01-07 12:25:20 +0100

    added function for submitting score

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const user1 = {
   name: "Karl",
   totalScore: 501,
-  score: 100,
+  score: 0,
   scores: [],
   isMyTurn: true,
   throwCounter: 0,
@@ -12,11 +12,44 @@ const user2 = {
   totalScore: 501,
   score: 0,
   scores: [],
-  isMyTurn: true,
+  isMyTurn: false,
   throwCounter: 0,
 };
 
 let calculateScore = (totalScore, score) => {
+  score = Number(document.getElementById("score-number").value);
   totalScore = totalScore - score;
   return totalScore;
 };
+
+// Check turn
+function submitScore() {
+  if (user1.isMyTurn) {
+    let userScore = Number(document.getElementById("score-number").value);
+    user1.score = userScore;
+    user1.totalScore = calculateScore(user1.totalScore, user1.score);
+    user1.throwCounter++;
+    user1.scores.push(user1.score);
+    user1.isMyTurn = false;
+    user2.isMyTurn = true;
+    let userSelect1 = document.getElementById("user1");
+    let userSelect2 = document.getElementById("user2");
+    userSelect1.classList.remove("turn");
+    userSelect2.classList.add("turn");
+    document.getElementById("user1-score").textContent = user1.totalScore;
+    console.log(user1.totalScore);
+  } else if (user2.isMyTurn) {
+    let userScore = Number(document.getElementById("score-number").value);
+    user2.score = userScore;
+    user2.totalScore = calculateScore(user2.totalScore, user2.score);
+    user2.throwCounter++;
+    user2.scores.push(user2.score);
+    user2.isMyTurn = false;
+    user1.isMyTurn = true;
+    let userSelect1 = document.getElementById("user1");
+    let userSelect2 = document.getElementById("user2");
+    userSelect1.classList.add("turn");
+    userSelect2.classList.remove("turn");
+    document.getElementById("user2-score").textContent = user2.totalScore;
+  }
+}
